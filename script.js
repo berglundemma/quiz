c4cb62f15cb00b085224601da5f23e69386bce36
Emma Berglund <emma.e.berglund@outlook.com>
2023-03-10 11:30:09 +0100

    init git

---
+++ script.js
@@ -0,0 +1,27 @@
+
+/*
+  1. Skapa en variabel där man håller koll på antalet rätt
+*/
+
+
+// 2. Skapa en variabel med ranken av spelaren
+
+
+/*
+  3. Skriv några frågor(som poppar upp på skärmen) och spara svaret i en variabel 
+*/
+
+
+/* 4. Kontrollera så att svaret är korrekt, om det är korrekt öka correct +=1 (++), if()*/ 
+
+/*
+  5. Ranka spelarna baserat på antal rätt
+   - 3 correct = Guld
+   - 2 correct = Silver
+   - 1 correct = Brons
+   - 0 correct = Ingen 
+   Kontrollera om correct är lika med 3 osv, lägg till rätt medalj i rank 
+*/
+
+/*6. Skriv till console, antalet rätt av 3 och vilken rank man fick*/ 
+
